3e0a2a5c883dbb9bf253856566b55cd4a9852e42
Junshan Wang <wjs9538@126.com>
2015-03-18 01:52:48 +0800

    完善登陆界面

--- src/main/webapp/drp/base/view/CenterView.js
+++ src/main/webapp/drp/base/view/CenterView.js
@@ -24,11 +24,11 @@ Ext.define('drp.base.view.CenterView', {
                   '<div style="line-height: 23px;"> '+
                     '<table width="1180" border="0" cellspacing="0" cellpadding="0" margin="10 0 10 30"><tbody><tr> '+
                         '<td width="170" height="100"> '+
-                            '&nbsp; <img src="'+'resources/images/footer/company.gif"></td> '+
+                            '&nbsp; <img src="'+'resources/images/footer/company.png"></td> '+
                         '<td> '+
-                            '地址(ADD)：宁夏固原市经济开发区<br> '+
-                            '电话(TEL)： 15202698249  官网 ：http://originspark.com.cn   邮箱: originspark@163.com<br> '+
-                            '页面版权所有&nbsp; 宁夏奥睿星火网络技术有限责任公司&nbsp;</td> '+
+                            '地址：宁夏固原市经济开发区<br> '+
+                            '电话：&nbsp; 15202698249&nbsp;  官网 ：http://originspark.com.cn&nbsp;   邮箱: originspark@163.com<br> '+
+                            '页面版权所有：&nbsp; 宁夏奥睿星火网络技术有限责任公司&nbsp; 技术支持：&nbsp; 奥睿星火&nbsp;</td> '+
                     '</tr></tbody></table></div></div></div>'
         }]
     }]
