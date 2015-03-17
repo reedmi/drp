b4baa21fe00e5ab7edc84d2c849c73e42d6c5df1
Junshan Wang <wjs9538@126.com>
2015-03-18 01:21:11 +0800

    tmp

--- src/main/webapp/drp/base/view/CenterView.js
+++ src/main/webapp/drp/base/view/CenterView.js
@@ -26,9 +26,9 @@ Ext.define('drp.base.view.CenterView', {
                         '<td width="170" height="100"> '+
                             '&nbsp; <img src="'+'resources/images/footer/company.gif"></td> '+
                         '<td> '+
-                            '地址(ADD)：济南市二环东路7151号港澳花园综合楼<br> '+
-                            '电话(TEL)：(0531) 85599617 85599618 传真(FAX)：(0531) 85599615 E-MAIL： wavetek@sdwavetek.com<br> '+
-                            '页面版权所有&nbsp; 山东华埠特克智能机电工程有限公司&nbsp;</td> '+
+                            '地址(ADD)：宁夏固原市经济开发区<br> '+
+                            '电话(TEL)： 15202698249  官网 ：http://originspark.com.cn   邮箱: originspark@163.com<br> '+
+                            '页面版权所有&nbsp; 宁夏奥睿星火网络技术有限责任公司&nbsp;</td> '+
                     '</tr></tbody></table></div></div></div>'
         }]
     }]
