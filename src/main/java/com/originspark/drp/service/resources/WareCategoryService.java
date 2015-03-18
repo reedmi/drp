d3bfe846aaf8458b3eb422d6e83c4881805f5d12
Yonggang Yuan <sdcsyyg@163.com>
2015-03-18 09:48:19 +0800

    用colume(nullable=false)代替@Notnull注解

--- src/main/java/com/originspark/drp/service/resources/WareCategoryService.java
+++ src/main/java/com/originspark/drp/service/resources/WareCategoryService.java
@@ -2,9 +2,6 @@ package com.originspark.drp.service.resources;
 
 
 import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.originspark.drp.dao.BaseDAO;
 import com.originspark.drp.models.resources.WareCategory;
 
